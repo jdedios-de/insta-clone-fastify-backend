8667551f08cce2b823388f01701ed9ae04cbdfea
Jerome de Dios <jeromededios.de@proton.me>
2025-09-21 20:55:40 +0200

    fix(posts): correct import of file-storage.service
    - updated posts.service.ts and posts.routes.ts to use correct relative paths
    
    - ensures module resolves properly on deployment (Vercel)

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import path from "path";
 
 import { databasePlugin } from "./core/database/database.plugin";
 import { postsRoutes } from "./modules/posts/posts.routes";
-import { reelsRoutes } from "src/modules/reels/reels.routes"
-import { taggedRoutes } from "src/modules/tagged/tagged.routes"
-import { highlightsRoutes } from "src/modules/highlights/highlights.routes"
+import { reelsRoutes } from "./modules/reels/reels.routes"
+import { taggedRoutes } from "./modules/tagged/tagged.routes"
+import { highlightsRoutes } from "./modules/highlights/highlights.routes"
 
 const fastify = Fastify({
     logger: true,
